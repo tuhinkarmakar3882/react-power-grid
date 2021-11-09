f0297e79e7b48b89ca8f528d0a2c5457d4399516
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-11-09 17:42:02 +0530

    Added Event Emitter whenever user clicks on the header column names

--- src/components/PowerTable.jsx
+++ src/components/PowerTable.jsx
@@ -2,11 +2,35 @@ import React from 'react'
 import '../styles/PowerTable.scss'
 
 const PowerTable = ({
-  headerColumns,
-  tableData
+  headerColumns = [],
+  tableData = [],
+  loading = false
 }) => {
+  const handleToggleAction = ({ target }) => {
+    const columnName = target.getAttribute('data-column-name')
+    const previousSortOrder = target.getAttribute('data-sort-order')
+    let newSortOrder = 'DESC'
+
+    if (!previousSortOrder || previousSortOrder === 'DESC') {
+      newSortOrder = 'ASC'
+    }
+
+    target.setAttribute('data-sort-order', newSortOrder)
+
+    const event = new CustomEvent('sort', {
+      bubbles: true,
+      detail: {
+        [columnName]: newSortOrder
+      }
+    })
+
+    target.dispatchEvent(event)
+  }
+
   const renderHeaderColumns = () => headerColumns.map(item => (
-    <section key={item.id} className="column">{item.name}</section>
+    <section key={item.id} className="column" data-column-name={item.name} data-sort-order={undefined}>
+      {item.name}
+    </section>
   ))
 
   const renderRows = () => tableData.map((row, rowIdx) => (
@@ -21,11 +45,17 @@ const PowerTable = ({
     </section>
   ))
 
+  const showSpinner = () => (<section className="loader"/>)
+
   return (
     <div className="power-table">
-      <header className="row">{renderHeaderColumns()}</header>
+      <header className="row" onClick={handleToggleAction}>
+        {renderHeaderColumns()}
+      </header>
 
-      <main>{renderRows()}</main>
+      <main>
+        {loading ? showSpinner() : renderRows()}
+      </main>
 
       <footer>
         <button>Previous</button>
