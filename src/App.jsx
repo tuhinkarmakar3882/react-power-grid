9e0a84ca1b8ae8988e820693e481cb501b7014bb
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-11-14 21:49:06 +0530

    Changed App.jsx Signature

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PowerTable from './components/PowerTable'
 import PowerTableTopBar from './components/PowerTable/PowerTableTopBar'
 import PowerTableHeader from './components/PowerTable/PowerTableHeader'
@@ -10,22 +10,22 @@ import PowerTableMoreFilters from './components/PowerTable/PowerTableMoreFilters
 const App = () => {
   const powerTableContainer = useRef(null)
 
-  const headerColumns = [
+  const [headerColumns] = useState([
     { id: 0, name: 'ID' },
     { id: 1, name: 'Name' },
     { id: 2, name: 'Age' },
     { id: 3, name: 'Phone number' },
     { id: 4, name: 'Something Else' }
-  ]
-  const tableData = [
-    ['1', 'SuperName', 25, '+919362145879', 'Apple'],
-    ['2', 'New Super Name', 5, '+919362145879', 'Ice Cream'],
-    ['3', 'SuperName', 25, '+919362145879', 'Ice Cream'],
-    ['4', 'SuperName', 15, '+919362145879', 'Ice Cream'],
-    ['5', 'SuperName', 75, '+919362145879', 'Ice Cream']
-  ]
-  const hasNext = true
-  const hasPrevious = false
+  ])
+  const [tableData, setTableData] = useState([
+    [true, '1', 'SuperName', 25, '+919362145879', 'Apple'],
+    [false, '2', 'New Super Name', 5, '+919362145879', 'Ice Cream'],
+    [true, '3', 'SuperName', 25, '+919362145879', 'Ice Cream'],
+    [false, '4', 'SuperName', 15, '+919362145879', 'Ice Cream'],
+    [false, '5', 'SuperName', 75, '+919362145879', 'Ice Cream']
+  ])
+  const hasNext = useState(true)
+  const hasPrevious = useState(true)
   const filter = [
     {
       name: 'Status',
@@ -57,19 +57,44 @@ const App = () => {
     }
   ]
 
-  const loadDataFromApi = (evt) => {
-    console.log(evt.detail)
+  const logEventDetails = (evt) => {
+    console.log(evt.type, evt.detail)
+  }
+
+  const handleIndividualRowSelection = (evt) => {
+    logEventDetails(evt)
+
+    const newTableData = [...tableData]
+    newTableData[evt.detail.rowIdx][0] = !newTableData[evt.detail.rowIdx][0]
+
+    setTableData(newTableData)
+  }
+
+  const handleAllRowSelection = (evt) => {
+    logEventDetails(evt)
+
+    setTableData(tableData.map(item => {
+      item[0] = evt.detail.checkedState
+      return item
+    }))
   }
 
   useEffect(() => {
-    powerTableContainer.current.addEventListener('sort', loadDataFromApi)
-    powerTableContainer.current.addEventListener('pagination', loadDataFromApi)
-    powerTableContainer.current.addEventListener('search', loadDataFromApi)
+    powerTableContainer.current.addEventListener('sort', logEventDetails)
+    powerTableContainer.current.addEventListener('pagination', logEventDetails)
+    powerTableContainer.current.addEventListener('search', logEventDetails)
+    powerTableContainer.current.addEventListener('toggle-row-selection', handleIndividualRowSelection)
+    powerTableContainer.current.addEventListener('select-all-columns', handleAllRowSelection)
+    powerTableContainer.current.addEventListener('dropdown-change', logEventDetails)
   }, [])
+
   useEffect(() => () => {
-    powerTableContainer.current.removeEventListener('sort', loadDataFromApi)
-    powerTableContainer.current.removeEventListener('pagination', loadDataFromApi)
-    powerTableContainer.current.removeEventListener('search', loadDataFromApi)
+    powerTableContainer?.current?.removeEventListener('sort', logEventDetails)
+    powerTableContainer?.current?.removeEventListener('pagination', logEventDetails)
+    powerTableContainer?.current?.removeEventListener('search', logEventDetails)
+    powerTableContainer?.current?.removeEventListener('toggle-row-selection', handleIndividualRowSelection)
+    powerTableContainer?.current?.removeEventListener('select-all-columns', handleAllRowSelection)
+    powerTableContainer?.current?.removeEventListener('dropdown-change', logEventDetails)
   }, [])
 
   return (
@@ -79,12 +104,18 @@ const App = () => {
         topBar={
           <PowerTableTopBar
             filters={filter}
-            moreFilters={<PowerTableMoreFilters moreFilters={undefined}/>}
+            moreFilters={
+              <PowerTableMoreFilters>
+                <h1>Put your more filters</h1>
+              </PowerTableMoreFilters>
+            }
             searchBar={<PowerTableSearchBar/>}
           />
         }
         tableHeader={<PowerTableHeader columns={headerColumns}/>}
-        tableBody={<PowerTableBody tableData={tableData}/>}
+        tableBody={
+          <PowerTableBody tableData={tableData}/>
+        }
         tableFooter={
           <PowerTableFooter
             hasNext={hasNext}
