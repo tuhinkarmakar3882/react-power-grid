5a6e4791de3b69eb0a9ff25a4cbc46452cdc585d
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-11-14 21:50:32 +0530

    Added Event Emission on RowSelect

--- src/components/PowerTable/PowerTableBody.jsx
+++ src/components/PowerTable/PowerTableBody.jsx
@@ -1,20 +1,45 @@
 import React from 'react'
 
 const PowerTableBody = ({ tableData = [] }) => {
+  const emitOnChangeEvent = ({ target }, rowIdx) => {
+    target.dispatchEvent(new CustomEvent('toggle-row-selection', {
+      bubbles: true,
+      detail: { rowIdx }
+    }))
+  }
+
   const renderRows = () => tableData.map((row, rowIdx) => (
     <section className="row" key={'row-' + rowIdx}>
       {renderColumns(row, rowIdx)}
     </section>
   ))
 
-  const renderColumns = (row, rowIdx) => row.map((column, columnIdx) => (
-    <section className="column" key={`row-${rowIdx} | column-${columnIdx}`}>
-      {column?.custom || column}
-    </section>
-  ))
+  const renderColumns = (row, rowIdx) => row.map((column, columnIdx) => {
+    const getColumnKey = `row-${rowIdx} | column-${columnIdx}`
+
+    const isFirstColumn = columnIdx === 0
+    const renderColumnItem = () => (column?.custom || column)
+
+    const renderCheckbox = () => (
+      <input type="checkbox" checked={column} onChange={(evt) => emitOnChangeEvent(evt, rowIdx)}/>
+    )
+
+    return (
+      <section className="column" key={getColumnKey}>
+        {isFirstColumn ? renderCheckbox() : renderColumnItem()}
+      </section>
+    )
+  })
+
+  const handleOnClick = ({ target }) => {
+    target.dispatchEvent(new CustomEvent('selectItem', {
+      bubbles: true,
+      detail: { hello: 'world' }
+    }))
+  }
 
   return (
-    <main>
+    <main onClick={handleOnClick}>
       {renderRows()}
     </main>
   )
