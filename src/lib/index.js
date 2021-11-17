f7149ef4485f7f4a7b0a5404e37a87aa5713bbf5
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-11-17 17:02:31 +0530

    Added Missing Imports
    Signed-off-by: Tuhin Karmakar <tuhinkarmakar3882@gmail.com>

--- src/lib/index.js
+++ src/lib/index.js
@@ -5,15 +5,17 @@ import PowerGridFooter from './components/PowerGrid/PowerGridFooter'
 import PowerGridHeader from './components/PowerGrid/PowerGridHeader'
 import PowerGridMoreFilters from './components/PowerGrid/PowerGridMoreFilters'
 import PowerGridSearchBar from './components/PowerGrid/PowerGridSearchBar'
+import PowerGridTopBar from './components/PowerGrid/PowerGridTopBar'
 import Spinner from './components/Spinner'
 
 export {
   PowerGrid,
-  PowerGridBody,
-  PowerGridDropdown,
-  PowerGridFooter,
+  PowerGridTopBar,
   PowerGridHeader,
-  PowerGridMoreFilters,
+  PowerGridBody,
+  PowerGridFooter,
   PowerGridSearchBar,
+  PowerGridMoreFilters,
+  PowerGridDropdown,
   Spinner
 }
