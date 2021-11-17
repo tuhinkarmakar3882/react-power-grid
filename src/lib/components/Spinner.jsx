f135ee633923255a30d60b68beabb32ba2af3c57
Tuhin Karmakar <tuhinkarmakar3882@gmail.com>
2021-11-17 16:31:49 +0530

    Replaced Global SCSS

--- src/lib/components/Spinner.jsx
+++ src/lib/components/Spinner.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import '../styles/Spinner.scss'
+import styles from '../styles/Spinner.scss'
 
-const Spinner = () => (<section className="loader"/>)
+const Spinner = () => (<section className={styles.loader}/>)
 
 export default Spinner
